795f0f2f663870a3e1174d38829dbe728ef8adb7
kunikada <kunikada@kunikada.com>
2021-09-11 17:53:41 +0900

    update config

--- www/application/config/config.php
+++ www/application/config/config.php
@@ -192,7 +192,7 @@ $config['log_threshold'] = 1;
 | application/logs/ folder. Use a full server path with trailing slash.
 |
 */
-$config['log_path'] = '/proc/self/fd/1';
+$config['log_path'] = '/dev/stdout';
 
 /*
 |--------------------------------------------------------------------------
